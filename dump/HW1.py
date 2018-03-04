f40946dd3a109eec96c40419e4ec805e904672ab
Kebur Fantahun <kebur.fantahun@ttu.edu>
2018-03-04 09:56:27 -0600

    small

---
+++ dump/HW1.py
@@ -0,0 +1,31 @@
+# Objective: read co-ordinates, plot points, print # of points, allow for STDIN
+# Make code flexible ~ specifically for #'s sep by commas, whitespace etc
+
+import numpy as np
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+# Specify input file by command line
+inputFile = input('\n' + 'Hi, please enter your x,y dataset filename for plotting: ')
+print('Gee thanks! I`ll have the plots for '+ inputFile + ' right out for you.') 
+
+# Print the number of points
+x = []
+y = []
+with open(inputFile) as f:
+
+	lines = f.readlines()	
+	for line in lines:
+		a = line.strip()
+		if a.startswith('#'):
+                        continue
+		if len(a) == 0:
+			continue
+		ttable = str.maketrans(',',' ')
+		b = a.translate(ttable).split()
+		x.append(float(b[0]))
+		y.append(float(b[1]))
+		#print(x[:10],y[:10])	
+	plt.scatter(x, y) 
+	plt.show()
