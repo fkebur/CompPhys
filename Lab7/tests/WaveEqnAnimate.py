2d2b996d8b760488e5d0847670ec1fef25ea625d
Kebur Fantahun <kebur.fantahun@ttu.edu>
2018-03-21 13:49:23 -0500

    l7 Mar 21

--- Lab7/tests/WaveEqnAnimate.py
+++ Lab7/tests/WaveEqnAnimate.py
@@ -23,7 +23,7 @@ x = linspace(0.0, string_length, n_spatial_points)
 dx = x[1] - x[0]
 
 # Simulation time step
-dt_factor = .8
+dt_factor = 1.4
 dt = dt_factor*abs(dx/c)
 
 # Total number of time steps for the simulation
@@ -40,11 +40,12 @@ solver = LaxAdvectionSolver1d(n_spatial_points, c, dt, dx, boundary_type)
 # Set up the initial values for the solver
 initial_values = triangle_wave(x, 1.0, 3.0, 2.0)
 initial_values2 = triangle_wave(x, 1.0, 3.0 +c*dt , 2.0)
+#initial_values3 = triangle_wave(x, 1.0, 3.0 +c*dt , 2.0)
 
 # initial_values = tringle_wave(x, 1.0, 3.0, 2.0)
 # initial_values = gaussian_wave(x, 1.0, 3.0, 1.0)
 solver.setInitialValues(initial_values)
-solver.setInitialValues(initial_values2, 1)
+solver.setInitialValues(initial_values2, 2)
 
 # Plot for the waveform
 ax = figure().add_subplot(111)
